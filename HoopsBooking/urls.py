645fd151a1d8cf3d194fc2dbc7541fef5ad395ff
Bogyi95 <krzysiek.bogusz@onet.pl>
2023-09-30 14:46:48 +0200

    Creating Login and Registration

--- HoopsBooking/urls.py
+++ HoopsBooking/urls.py
@@ -24,4 +24,14 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", user_views.home, name="home"),
     path("register/", user_views.register, name="register"),
+    path(
+        "login/",
+        auth_views.LoginView.as_view(template_name="login.html"),
+        name="login",
+    ),
+    path(
+        "logout/",
+        auth_views.LogoutView.as_view(template_name="logout.html"),
+        name="logout",
+    ),
 ]
--- users/views.py
+++ users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegistrationForm
 
 
 def home(request):
@@ -10,7 +10,7 @@ def home(request):
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
 
@@ -19,7 +19,7 @@ def register(request):
             )
             return redirect("login")
     else:
-        form = UserCreationForm()
+        form = UserRegistrationForm()
 
     context = {"form": form}
     return render(request, "register.html", context)
