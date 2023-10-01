f631bdda40681ed2bc10a840eea77535f176e042
Bogyi95 <krzysiek.bogusz@onet.pl>
2023-10-01 13:16:48 +0200

    displaying teams

--- users/views.py
+++ users/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
-
+from django.db.models import Q
 from .forms import UserRegistrationForm, TeamForm
+from .models import Team
 
 
 def home(request):
-    return render(request, "home.html")
+    teams = Team.objects.all()
+
+    context = {"teams": teams}
+    return render(request, "home.html", context)
 
 
 def register(request):
@@ -30,8 +34,20 @@ def create_team(request):
     if request.method == "POST":
         form = TeamForm(request.POST)
         if form.is_valid():
-            form.save()
-            return redirect("home")
+            team_name = form.cleaned_data["name"]
+
+            if Team.objects.filter(Q(name__iexact=team_name)).exists():
+                form.add_error("name", "A team with this name already exists.")
+            else:
+                form.save()
+                return redirect("home")
     else:
         form = TeamForm()
-    return render(request, "create_team.html", {"form": form})
+    return render(request, "home.html", {"form": form})
+
+
+def show_teams(request):
+    teams = Team.objects.all()
+
+    context = {"teams": teams}
+    return render(request, home.html, context)
